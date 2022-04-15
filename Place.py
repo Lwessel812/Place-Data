0b84ff71c38637d2dcb19526d67fe96132291dfe
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2022-04-15 10:27:53 -0500

    1

--- Place.py
+++ Place.py
@@ -1,22 +1,10 @@
 import pandas as pd
 import os
 
-# 160353103 rows
+# 160353103-4 rows
 
 os.system ('cls' if os.name == 'nt' else 'clear')
 print("==========================================\n")
 filename = "CutSet.csv"
 
 data = pd.read_csv(filename)
-
-#print(f"Original {filename} CSV Data: \n")
-#print(data)
-
-#data.drop("user_id", inplace=True, axis=1)
-
-#print("\nCSV Data after deleting the column 'user_id':\n")
-#print(data)
-
-#data.to_csv("CutSet.csv", index=False)
-
-#print(f"# Rows: {len(data.index)}")
--- Test.py
+++ Test.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import os
 
+# 160353103-4 rows
+
 os.system ('cls' if os.name == 'nt' else 'clear')
 print("==========================================\n")
-filename = "E:\GitHub\CutSet.csv"
+filename = "CutSet.csv"
 
 data = pd.read_csv(filename)
 
-print(f"Original {filename} CSV Data: \n")
-print(data)
+#print(f"Original {filename} CSV Data: \n")
+#print(data)
 
 #data.drop("user_id", inplace=True, axis=1)
 
 #print("\nCSV Data after deleting the column 'user_id':\n")
 #print(data)
+
+#data.to_csv("CutSet.csv", index=False)
+
+#print(f"# Rows: {len(data.index)}")
